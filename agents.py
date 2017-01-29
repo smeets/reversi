003c0c6b543a9bef2c80733eb4b4999ab9fd393a
Axel Smeets <murlocbrand@gmail.com>
2017-01-29 22:53:39 +0100

    Unix style linendings + tab=space

--- agents.py
+++ agents.py
@@ -2,26 +2,26 @@ import random
 
 class Agent:
 
-	names = ["Max", "Oscar", "Bob", "Charles"]
+    names = ["Max", "Oscar", "Bob", "Charles"]
 
-	def __init__(self):
-		self.name = random.choice(Agent.names)
+    def __init__(self):
+        self.name = random.choice(Agent.names)
 
-	def next_move(self, game, state):
-		abstract
+    def next_move(self, game, state):
+        abstract
 
 class RandomAgent(Agent):
 
-	def next_move(self, game, state):
-		moves = game.legal_moves(state)
-		return "pass" if not moves else random.choice(moves)
+    def next_move(self, game, state):
+        moves = game.legal_moves(state)
+        return "pass" if not moves else random.choice(moves)
 
 class MinMaxAgent(Agent):
 
-	def next_move(self, game, state):
-		return (1, 1)
+    def next_move(self, game, state):
+        return (1, 1)
 
 class AlphaBetaAgent(Agent):
 
-	def next_move(self, game, state):
-		return (1, 1)
+    def next_move(self, game, state):
+        return (1, 1)
--- runner.py
+++ runner.py
@@ -2,18 +2,18 @@ from reversi import Reversi, move_repr, BLACK, WHITE
 from agents import RandomAgent
 
 def play_game(game, agents):
-	moves = []
-	state = game.initial_state()
-	while True:
-		passes = 0
-		for agent in agents:
-			move = agent.next_move(game, state)
-			state = game.make_move(state, move)
-			moves.append(move)
-			if game.over(state):
-				passes += 1
-		if passes == len(agents):
-			return state, moves
+    moves = []
+    state = game.initial_state()
+    while True:
+        passes = 0
+        for agent in agents:
+            move = agent.next_move(game, state)
+            state = game.make_move(state, move)
+            moves.append(move)
+            if game.over(state):
+                passes += 1
+        if passes == len(agents):
+            return state, moves
 
 game = Reversi()
 agents = [RandomAgent(), RandomAgent()]
@@ -22,7 +22,7 @@ state, moves = play_game(game, agents)
 winner, score = game.top_scoring_player(state)
 
 #for move in moves:
-#	print move_repr(move)
+#   print move_repr(move)
 
 game.print_board(state)
 print "Winner is", winner, "at turn", state["turn"], "with", score, "score!"
