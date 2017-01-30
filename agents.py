ca01deb052de07859ee89c373d1f00976bcce7a2
GustavSvensson3600 <gustavsp91@gmail.com>
2017-01-30 14:40:41 +0100

    Perhaps not sane approach to it, instead of building tree recursive method is used
    Signed-off-by: GustavSvensson3600 <gustavsp91@gmail.com>

--- agents.py
+++ agents.py
@@ -24,7 +24,64 @@ class MinMaxAgent(Agent):
         # then we can recursively descend search the tree and
         # propagate min-max values upwards so that we finally
         # can select the best move.
-        return (1, 1)
+        
+        moves = game.legal_moves(state);
+        if not moves
+            return "pass"
+        else
+            depth = 6
+            values = dict()
+            for move in moves:
+                temp_state = game.make_move(state, move)
+                value = min_val(self, game, temp_state, depth)
+                values[move] = value
+            return max(values, key=values.get)
+        
+
+    def max_val(self, game, state, depth):
+        if depth = 0
+            return heuristics()
+        else
+            moves = game.legal_moves(state);
+            if not moves
+                #We hit terminus
+                #Return something
+                #Win / loose
+                #Or score?
+            else 
+                depth -= 1
+                values = dict()
+                for move in moves:
+                    temp_state = game.make_move(state, move)
+                    value = min_val(self, game, temp_state, depth)
+                    values[move] = value
+                return values[max(values, key=values.get)]
+
+
+    def min_val(self, game, state, depth):
+        if depth = 0
+            return heuristics()
+        else
+            moves = game.legal_moves(state);
+            if not moves
+                #We hit terminus
+                #Return something
+                #Win / loose
+                #Or score?
+                return 0;
+            else 
+                depth -= 1
+                values = dict()
+                for move in moves:
+                    temp_state = game.make_move(state, move)
+                    value = max_val(self, game, temp_state, depth)
+                    values[move] = value
+                return values[min(values, key=values.get)]
+
+    def heuristics(self, game, state):
+        #return some kind of integer value
+        return 0
+
 
 class AlphaBetaAgent(Agent):
 
